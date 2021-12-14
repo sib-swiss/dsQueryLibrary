cebb31c88990b28de0eb7525d83d87b20915a971
IulianD <iulian_dragan@yahoo.com>
2021-12-14 08:26:32 +0100

    union in dsqLoad

--- R/dsqLoad.R
+++ R/dsqLoad.R
@@ -1,23 +1,27 @@
 #' @title Load the result of an OMOP query in the remobe session(s)
 #' @description Load remotely one of the preset queries (previously retrieved with dsqShowQueries)
-#' @param symbol a character, the name of the dataframe where the resultset will be loaded. It defaults to the query_name.
+#' @param symbol a character, the name of the data frame where the resultset will be loaded. It defaults to the query_name.
 #' @param domain a character, the query domain (ex. 'care_site')
 #' @param query_name the query name as it appears in the result of dsqShowQueries
 #' @param where_clause an optional where clause that can be appended to the query (without the 'where' keyword)
 #' @param row_limit maximum number of row retrieved
-#' @param db_connection a character, the name of the connection to the database. This must exist in the remote session(s) - it can be created with datashield.assign.resource().
-#' If no db_connection is provided, the first connection found in the renote session (alphabetically) will be used.
+#' @param db_connections a vector, the name of the connection(s) to the database. They must exist in the remote session(s) - it can be created with datashield.assign.resource().
+#' If no db_connection is provided, all the connections found in the renote session will be used. One more column called "database"
+#' will be added to the query results and it will be populated with the respective connection names. The next argument (union)
+#' will goevern further behaviour.
+#' @param union a logical, if TRUE (the default), the resultsets across the multiple connections will be concatenated in one single data frame.
+#' If FALSE, one data frame will be created for each connection (with the connection name concatenated to the original name)
 #' @param async same as in datashield.assign
 #' @param datasources same as in datashield.assign
 #' @return the query result
 #' @export
-dsqLoad <- function (symbol = NULL, domain = NULL, query_name = NULL, where_clause = NULL, row_limit = NULL, db_connection = NULL, async = TRUE, datasources = NULL){
+dsqLoad <- function (symbol = NULL, domain = NULL, query_name = NULL, where_clause = NULL, row_limit = NULL, db_connections = NULL, union = TRUE, async = TRUE, datasources = NULL){
  
   
   if (is.null(datasources)) {
     datasources <- datashield.connections_find()
   }
-  expr <- list(as.symbol('execQuery'),  domain, query_name, NULL, symbol, dsSwissKnifeClient:::.encode.arg(where_clause), row_limit, db_connection)
+  expr <- list(as.symbol('execQuery'),  domain, query_name, NULL, symbol, dsSwissKnifeClient:::.encode.arg(where_clause), row_limit, db_connections, union)
   datashield.aggregate(datasources,as.call(expr), async=async)
   
 }
